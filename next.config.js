10de1c929820c03516ca70642ac9a9d42985d1ed
Andry Pebrianto <andrypeb227@gmail.com>
2022-06-25 01:21:15 +0700

    Coba fix lagi

--- next.config.js
+++ next.config.js
@@ -7,9 +7,8 @@ const nextConfig = {
       "images227.netlify.app",
       "drive.google.com",
       "ankasa-ticket.herokuapp.com",
-      "https://drive.google.com/drive/u/0/folders/1FySMSQU2Y0qECTQbNOVMTto5nQA0Ex6a",
+      "drive.google.com/drive/u/0/folders/1FySMSQU2Y0qECTQbNOVMTto5nQA0Ex6a",
     ],
-    formats: ["image/webp", "image/png", "image/jpg", "image/jpeg"],
   },
 };
 
